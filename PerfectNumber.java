bf13a0cdcb48c5d3316ab72414bed82f84ded119
thashingowda <109234285+thashingowda@users.noreply.github.com>
2024-04-25 11:30:12 +0530

    Add files via upload

---
+++ PerfectNumber.java
@@ -0,0 +1,17 @@
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int n=6;
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                sum=sum+i;
+                if(sum==n){
+                    System.out.println("perfect number");
+                }
+            }
+        }
+
+    }
+}
